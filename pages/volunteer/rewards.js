2d511a7a15f1890fd5342fededc1e0314ed54882
yongjii <nyjyongjie@gmail.com>
2022-07-10 05:07:45 +0800

    Update rewards.js

--- pages/volunteer/rewards.js
+++ pages/volunteer/rewards.js
@@ -22,100 +22,47 @@ import {
           boxShadow={'md'}
           rounded={'xl'}
           p={6}
-          textAlign={'center'}>
+          textAlign={'left'}>
             <Center>
               <Image
               rounded={'xl'}
-              width={'280px'}
+              width={'300px'}
+              height={'220px'}
+              objectFit='cover'
               src={
-                'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+                'https://www.paragon.com.sg/media/media/images/stores/Starbucks/starbucks_logo.jpg'
               }
               ></Image>
             </Center>
-          <Avatar
-            size={'xl'}
-            src={
-              'https://images.unsplash.com/photo-1520810627419-35e362c5dc07?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
-            }
-            alt={'Avatar Alt'}
-            mb={4}
-            pos={'relative'}
-            _after={{
-              content: '""',
-              w: 4,
-              h: 4,
-              bg: 'green.300',
-              border: '2px solid white',
-              rounded: 'full',
-              pos: 'absolute',
-              bottom: 0,
-              right: 3,
-            }}
-          />
+            <br></br>
           <Heading fontSize={'2xl'} fontFamily={'body'}>
-            Yong James
+            $10 Starbucks Gift Card
           </Heading>
-          <Text py={1} fontWeight={600} color={'gray.500'} mb={4}>
-            yongjames@email.com
-          </Text>
-          <Text
-            textAlign={'center'}
-            color={useColorModeValue('gray.700', 'gray.400')}
-            px={3}>
-            Contact: 97365732
-          </Text>
-  
-          <Stack align={'center'} justify={'center'} direction={'row'} mt={6}>
-            <Text>Skills:</Text>
-            <Badge
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              cooking
-            </Badge>
-            <Badge
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              photography
-            </Badge>
-            <Badge
-              px={2}
-              py={1}
-              bg={useColorModeValue('gray.50', 'gray.800')}
-              fontWeight={'400'}>
-              music
-            </Badge>
-          </Stack>
   
           <Stack mt={8} direction={'row'} spacing={4}>
+          <Badge
+              px={3}
+              py={3}
+              bg={useColorModeValue('gray.50', 'gray.800')}
+              fontWeight={'600'}>
+              200 Points
+            </Badge>
             <Button
               flex={1}
               fontSize={'sm'}
               rounded={'full'}
-              _focus={{
-                bg: 'gray.200',
-              }}>
-              Message
-            </Button>
-            <Button
-              flex={1}
-              fontSize={'sm'}
-              rounded={'full'}
-              bg={'blue.400'}
+              bg={'primary.100'}
               color={'white'}
               boxShadow={
                 '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
               }
               _hover={{
-                bg: 'blue.500',
+                bg: 'primary.200',
               }}
               _focus={{
-                bg: 'blue.500',
+                bg: 'primary.100',
               }}>
-              Follow
+              Redeem
             </Button>
           </Stack>
         </Box>
