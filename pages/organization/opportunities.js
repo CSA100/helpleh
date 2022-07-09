d5e124c2983e3113f65dfde8fa324cea2ad11ba3
Lebarnon <53988015+Lebarnon@users.noreply.github.com>
2022-07-10 04:24:37 +0800

    started on org listing

--- pages/organization/opportunities.js
+++ pages/organization/opportunities.js
@@ -1,1 +1,36 @@
-export default function Opportunities() {}
+import OppCard from "../../components/oppcard";
+import OppDetails from "../../components/oppDetails";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useEffect, useState } from "react";
+import { useAuth } from "../context/AuthUserContext";
+
+export default function Opportunities() {
+  const { authUser } = useAuth();
+  const [opps, setOpps] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const querySnapshot = await getDocs(
+        collection(db, "OpportunityListings"),
+        where("orgId", "==", authUser.uid)
+      );
+      const opplist = [];
+      querySnapshot.forEach((doc) => {
+        const obj = doc.data();
+        obj["id"] = doc.id;
+        opplist.push(obj);
+      });
+      setOpps(opplist);
+    })();
+  }, []);
+  return (
+    <>
+      {opps.map((opp) => (
+        <OppCard key={opp.id} opp={opp} >
+          <OppDetails key={opp.id} opp={opp}/>
+        </OppCard>
+      ))}
+    </>
+  );
+}
